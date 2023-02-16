56185d63f14915f1646053ea145bf91b0801871a
Ayush <ayushjain201474@gmail.com>
2023-02-16 21:09:09 +0530

    Completed Assignment 3.1

--- Week-3/Assignment 3.1/memoize.js
+++ Week-3/Assignment 3.1/memoize.js
@@ -1,1 +1,32 @@
-console.log("hello")
+//Given reducer method:
+function add(a, b) {
+  return b === undefined ? a : a + b;
+}
+
+//Create a method called memoize such that:
+const memoizeAdd = memoize(add);
+
+console.time("test_1");
+console.log(memoizeAdd(100, 100)); //returns 200
+console.timeEnd("test_1");
+console.log(memoizeAdd(100)); //returns 100
+console.log(memoizeAdd(100, 200)); //returns 300
+console.time("test_2");
+console.log(memoizeAdd(100, 100)); //returns 200 without computing
+console.timeEnd("test_2");
+
+// Memoize function
+function memoize(func) {
+  const memory = new Map();
+
+  return function (...args) {
+    const key = JSON.stringify([...args]);
+
+    if (memory.has(key)) {
+      return memory.get(key);
+    }
+    const result = func(...args);
+    memory.set(key, result);
+    return result;
+  };
+}
